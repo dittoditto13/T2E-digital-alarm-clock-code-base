6955b4abeb91180f720ea37d55014b03213820cf
dittoditto13 <dittoditto8002@gmail.com>
2026-03-30 07:47:38 -0400

    Delete ClockNumClear.m

--- ClockNumClear.m
+++
@@ -1,8 +0,0 @@
-function ClockNumClear(system,selectPins,segmentPins)
-    for k = 1:4
-        writeDigitalPin(system,selectPins{k}, 1);
-    end
-    for k = 1:8
-        writeDigitalPin(system,segmentPins{k}, 0);
-    end
-end
